926372304bac6dbc97cdf1d4559f7118557bb985
Thomas Tannenberg <Thomas@Tannenberg.online>
2023-11-13 20:35:56 +0100

    JavaFX added

--- src/main/java/uebung1/RechnerTest.java
+++ src/main/java/uebung1/RechnerTest.java
@@ -23,13 +23,16 @@ public class RechnerTest {
 
         } finally {
             scanner.close();
+
+            System.out.println("Addition: " + Rechner.addieren(a, b));
+            System.out.println("Subtraktion: " + Rechner.subtrahieren(a, b));
+            System.out.println("Multiplikation: " + Rechner.multiplizieren(a, b));
+            System.out.println("Division: " + Rechner.dividieren(a, b));
+
             System.out.println("Bye Bye!");
         }
 
-        System.out.println("Addition: " + Rechner.addieren(a, b));
-        System.out.println("Subtraktion: " + Rechner.subtrahieren(a, b));
-        System.out.println("Multiplikation: " + Rechner.multiplizieren(a, b));
-        System.out.println("Division: " + Rechner.dividieren(a, b));
+
 
     }
 }
